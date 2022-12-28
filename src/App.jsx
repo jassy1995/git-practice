353c801899a25d82d4235fc7dadf673fb3ce717b
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-12-28 20:33:35 +0100

    added the use reducer example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Users from "./pages/users"
+import ReducerExample from "./pages/counter"
 
 function App() {
 
   return (
     <div className="container mx-auto">
       <Users />
+      <hr className="my-1" />
+      <ReducerExample />
     </div>
   )
 }
