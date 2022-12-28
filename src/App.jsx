0277f46f6b5a6b478ab60acf49772023c817a2c1
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-12-28 20:16:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Users from "./pages/users"
+
+function App() {
+
+  return (
+    <div className="container mx-auto">
+      <h1>user list</h1>
+      <Users />
+    </div>
+  )
+}
+
+export default App
